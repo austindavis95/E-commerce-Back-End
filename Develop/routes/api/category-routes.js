60db065d8518f11f9492f35ef7160d906f288ca3
Austin Davis <austindavis@Austins-MBP.attlocal.net>
2022-10-09 01:01:31 -0400

    building routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,12 +6,51 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name', 'category_id', 'stock', 'price']
+      }
+    ]
+  })
+
+  .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  Category.findOne({
+    
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Product,
+          attributes: ['product_name', 'price', 'stock', 'category_id']
+        },
+      ]
+    })
+  
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category was found with this id!' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
 
 router.post('/', (req, res) => {
   // create a new category
